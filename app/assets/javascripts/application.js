d2374af9d21a2e925423b895a290387e174fd770
Akhil Raju <akhil777raju@gmail.com>
2026-08-16 19:55:59 +1000

    home page frontend done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,7 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+// Loads all Semantic javascripts
+//= require semantic-ui
+//= require jquery
---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,6 @@
+class ChatroomController < ApplicationController
+    
+    def new
+    end
+    
+end
